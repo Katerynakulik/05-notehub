020d3891f0ced5ad861a4887d0b9789523f26e93
Katerynakulik <katerynakulik4@gmail.com>
2025-10-15 12:31:50 +0300

    Create structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import css from "./App.module.css";
+
+function App() {
+  return (
+    <>
+      <div className={css.app}>
+        <header className={css.toolbar}>
+          {/* Компонент SearchBox */}
+          {/* Пагінація */}
+          {/* Кнопка створення нотатки */}
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
