e4d8759844812add6c832f5b67e8cb31452fa09d
Katerynakulik <katerynakulik4@gmail.com>
2025-10-16 15:13:25 +0300

    Create SearchBox

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,1 +1,23 @@
 import css from "./SearchBox.module.css";
+import type { DebouncedState } from "use-debounce";
+
+interface Props {
+  onSearch: DebouncedState<(val: string) => void>;
+  value: string;
+}
+const SearchBox = ({ onSearch, value }: Props) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onSearch(e.target.value);
+  };
+  return (
+    <input
+      className={css.input}
+      type="text"
+      defaultValue={value}
+      onChange={handleChange}
+      placeholder="Search notes"
+    />
+  );
+};
+
+export default SearchBox;
